949043d76a2cc83d0d5f1b8d8b10438d9d8d9695
EmilianaAP <EmilianaAP>
2024-03-11 09:12:15 +0200

    All pages added to nodejs server

--- server.mjs
+++ server.mjs
@@ -1,10 +1,29 @@
 import express from "express";
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+// Get the directory path of the current module
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const filePath = path.resolve(__dirname, 'main-page.html');
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+    res.sendFile(filePath);
+});
+
+app.get('/login-page.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'login-page.html'));
+});
+
+app.get('/register-page.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'register-page.html'));
+});
+
+app.get('/profile-page.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'profile-page.html'));
 });
 
 app.listen(port, () => {

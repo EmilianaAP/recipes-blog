6fbe828cab24158a545ff85e8b9842f2fded5fdd
EmilianaAP <EmilianaAP>
2024-03-26 10:52:10 +0200

    Added stuff to the SQLite (Adding table if not exist, inserting into table and selecting from table)

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, (err) => {
 
     console.log('Connection successful');
 });
+
+db.run('CREATE TABLE IF NOT EXISTS Users(email, password, role, userID)')
+
+//const sql = 'INSERT INTO Users (email, password, role, userID) VALUES(?,?,?,?)';
+
+/*db.run(sql, ['ema.andr33va@gmail.com', '12341234', 'admin', 0], (err) => {
+    if (err) return console.error(err.message);
+
+    console.log("A new row has been created");
+});*/
+
+const sql = 'SELECT * FROM Users';
+
+db.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+
+    rows.forEach((row) => {
+        console.log(row);
+    })
+})
+
+db.close((err) => {
+    if (err) return console.error(err.message);
+})
